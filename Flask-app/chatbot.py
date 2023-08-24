85c227cd04b73cda629cf1cc3d6733285cf3e575
hamlet0808 <hamletxachatryan08@gmail.com>
2023-08-24 18:49:30 +0400

    "Added final version"

--- Flask-app/chatbot.py
+++ Flask-app/chatbot.py
@@ -5,7 +5,7 @@ from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
-    MessagesPlaceholder
+    MessagesPlaceholder,
 )
 
 from langchain.embeddings import OpenAIEmbeddings
@@ -19,38 +19,46 @@ class Chatbot:
     def __init__(self, model_name):
 
         self.model_name = model_name
-        self.embeddings = OpenAIEmbeddings(model='text-embedding-ada-002')
+        self.embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
         self.llm = ChatOpenAI(model_name=model_name, temperature=0.0, max_tokens=500)
         self.memory = ConversationBufferWindowMemory(k=3, return_messages=True)
         self.chat_history = []
+        self.template_v5 = """
+                              Act as a helpful startup legal assistant. Your name is Jessica.
+                              Greeting: If user says his name address them by name.
+                              Introduction: Say your name and introduce yourself as a startup legal assistant.
+                              Use provided context to answer the questions.
+                              If the question is about the sources of your context, just say "As an AI language model, I draw upon a large pool of data and don't rely on any one single source."
+                              VERY IMPORTANT: Do Not disclose your sources.
+                              VERY IMPORTANT: Respond as if you are talking with a non-lawyer.
+                              VERY IMPORTANT: If the question is not related to startup law and is NOT a greeting, just say "Hmm, I don't think this question is about startup law. I can only provide insights on startup law.  Sorry about that!".
+                           """
 
     def get_vectorstore(self, key, env, index_name):
         """
         Load vectorstore of our data from Pinecone index
         """
-        pinecone.init(api_key=key,
-                      environment=env)
+        pinecone.init(api_key=key, environment=env)
 
         vectorstore = Pinecone.from_existing_index(
-                                embedding=self.embeddings,
-                                index_name=index_name)
+            embedding=self.embeddings, index_name=index_name
+        )
 
         return vectorstore
 
-    def get_vectorstore_deeplake(self, path):
-        """
-        Load vectorstore of our data from Deeplake path
-        """
-        vectorstore_dl = DeepLake(
-                    dataset_path=path, embedding_function=self.embeddings, read_only=True
-            )
-
-        return vectorstore_dl
+    # def get_vectorstore_deeplake(self, path):
+    #     """
+    #     Load vectorstore of our data from Deeplake path
+    #     """
+    #     vectorstore_dl = DeepLake(
+    #         dataset_path=path, embedding_function=self.embeddings, read_only=True
+    #     )
+    #
+    #     return vectorstore_dl
 
     def query_refiner(self, query, api_key):
-
         conversation = self.chat_history[-2:]
-        #print("conversation", conversation)
+        # print("conversation", conversation)
         openai.api_key = api_key
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -59,35 +67,36 @@ class Chatbot:
             max_tokens=256,
             top_p=1,
             frequency_penalty=0,
-            presence_penalty=0
+            presence_penalty=0,
         )
 
-        response_text = response['choices'][0]['text']
+        response_text = response["choices"][0]["text"]
 
         return response_text
-
 
     def conversational_chat(self):
         """
         Start a conversational chat with a model via Langchain
         """
-        system_msg_template = SystemMessagePromptTemplate.from_template(template="""
-                      Act as a helpful startup legal assistant. Your name is Jessica.
-                      Greeting: If user says his name address them by name.
-                      Introduction: Say your name and introduce yourself as a startup legal assistant.
-                      VERY IMPORTANT: Do Not disclose your sources.
-                      VERY IMPORTANT: Do Not disclose any names of persons or names of organizations in your response.
-                      VERY IMPORTANT: If the question is about the sources of your context, just say "As an AI language model, I draw upon a large pool of data and don't rely on any one single source."
-                      VERY IMPORTANT: Respond as if you are talking with a non-lawyer.
-                      VERY IMPORTANT: Use provided context to answer the question below.
-                      """)
 
-        human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
-        qa_prompt = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+        system_msg_template = SystemMessagePromptTemplate.from_template(
+                template=self.template_v5
+            )
 
-        chain = ConversationChain(llm=self.llm, 
-                                  prompt=qa_prompt,
-                                  memory=self.memory,
-                                  verbose=True)
+        human_msg_template = HumanMessagePromptTemplate.from_template(
+            template="{input}"
+        )
+
+        qa_prompt = ChatPromptTemplate.from_messages(
+            [
+                system_msg_template,
+                MessagesPlaceholder(variable_name="history"),
+                human_msg_template,
+            ]
+        )
+
+        chain = ConversationChain(
+            llm=self.llm, prompt=qa_prompt, memory=self.memory, verbose=True
+        )
 
         return chain
